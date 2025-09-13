431353a4e1f21a2ccfeb6aff6be0840bc40611c5
L QJ <96654617+2PuceWriter79890@users.noreply.github.com>
2025-09-13 21:43:01 +0800

    Delete src/mod/Main.cpp

--- src/mod/Main.cpp
+++
@@ -1,7 +0,0 @@
-#include "mod/ChancePlugin.h"
-#include <ll/api/mod/NativeMod.h>
-
-extern "C" LL_EXPORT bool ll_mod_init(ll::mod::NativeMod& mod) {
-    chance_plugin::ChancePlugin::getInstance(mod);
-    return true;
-}
